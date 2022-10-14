b09d99ca7f0c999731a774a92c7c96fdb604638e
Jason Walton <jwalton@solinkcorp.com>
2022-10-14 15:42:50 -0400

    chore: Upgrade dev dependencies.

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,4 @@
 module.exports = {
-    globals: {
-        'ts-jest': {
-            // Tell ts-jest about our typescript config.
-            // You can specify a path to your tsconfig.json file,
-            // but since we're compiling specifically for node here,
-            // this works too.
-            tsconfig: {
-                target: 'es2019',
-            },
-        },
-    },
     // Transforms tell jest how to process our non-javascript files.
     // Here we're using babel for .js and .jsx files, and ts-jest for
     // .ts and .tsx files.  You *can* just use babel-jest for both, if
